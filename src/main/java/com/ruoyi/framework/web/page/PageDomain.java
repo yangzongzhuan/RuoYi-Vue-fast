4f2baec6d0782840cb1b22bea70407286354158a
RuoYi <yzz_ivy@163.com>
2020-08-19 16:59:40 +0800

    设置默认排序顺序

--- src/main/java/com/ruoyi/framework/web/page/PageDomain.java
+++ src/main/java/com/ruoyi/framework/web/page/PageDomain.java
@@ -11,12 +11,15 @@ public class PageDomain
 {
     /** 当前记录起始索引 */
     private Integer pageNum;
+
     /** 每页显示记录数 */
     private Integer pageSize;
+
     /** 排序列 */
     private String orderByColumn;
-    /** 排序的方向 "desc" 或者 "asc". */
-    private String isAsc;
+
+    /** 排序的方向desc或者asc */
+    private String isAsc = "asc";
 
     public String getOrderBy()
     {
