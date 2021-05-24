9cb33f4d6d4f36791d67a6d543b5a170f71a1f8a
RuoYi <yzz_ivy@163.com>
2021-05-24 11:38:47 +0800

    去除job多余的Serializable

--- src/main/java/com/ruoyi/project/monitor/domain/SysJob.java
+++ src/main/java/com/ruoyi/project/monitor/domain/SysJob.java
@@ -1,6 +1,5 @@
 package com.ruoyi.project.monitor.domain;
 
-import java.io.Serializable;
 import java.util.Date;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -19,7 +18,7 @@ import com.ruoyi.framework.web.domain.BaseEntity;
  * 
  * @author ruoyi
  */
-public class SysJob extends BaseEntity implements Serializable
+public class SysJob extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
 
