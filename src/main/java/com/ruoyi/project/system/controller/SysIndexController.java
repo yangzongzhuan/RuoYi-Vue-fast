8dee57fb975aab4e373924a7b0ad4516b25b5ee0
RuoYi <yzz_ivy@163.com>
2021-07-11 15:22:47 +0800

    新增默认访问首页的提示语

---
+++ src/main/java/com/ruoyi/project/system/controller/SysIndexController.java
@@ -0,0 +1,29 @@
+package com.ruoyi.project.system.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.framework.config.RuoYiConfig;
+
+/**
+ * 首页
+ *
+ * @author ruoyi
+ */
+@RestController
+public class SysIndexController
+{
+    /** 系统基础配置 */
+    @Autowired
+    private RuoYiConfig ruoyiConfig;
+
+    /**
+     * 访问首页，提示语
+     */
+    @RequestMapping("/")
+    public String index()
+    {
+        return StringUtils.format("欢迎使用RuoYi后台管理框架，当前版本：v{}", ruoyiConfig.getVersion());
+    }
+}
