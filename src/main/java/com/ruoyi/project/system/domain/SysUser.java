fd0de5c3ce1b0faa0f2e274d7b70f516e69d575a
RuoYi <yzz_ivy@163.com>
2021-07-28 16:17:27 +0800

    优化XSS跨站脚本过滤

--- src/main/java/com/ruoyi/project/system/domain/SysUser.java
+++ src/main/java/com/ruoyi/project/system/domain/SysUser.java
@@ -58,9 +58,6 @@ public class SysUser extends BaseEntity
     /** 密码 */
     private String password;
 
-    /** 盐加密 */
-    private String salt;
-
     /** 帐号状态（0正常 1停用） */
     @Excel(name = "帐号状态", readConverterExp = "0=正常,1=停用")
     private String status;
@@ -213,16 +210,6 @@ public class SysUser extends BaseEntity
         this.password = password;
     }
 
-    public String getSalt()
-    {
-        return salt;
-    }
-
-    public void setSalt(String salt)
-    {
-        this.salt = salt;
-    }
-
     public String getStatus()
     {
         return status;
@@ -325,7 +312,6 @@ public class SysUser extends BaseEntity
             .append("sex", getSex())
             .append("avatar", getAvatar())
             .append("password", getPassword())
-            .append("salt", getSalt())
             .append("status", getStatus())
             .append("delFlag", getDelFlag())
             .append("loginIp", getLoginIp())
