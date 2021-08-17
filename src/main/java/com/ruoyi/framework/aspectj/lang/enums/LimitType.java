b8f7b48b79e496c3d4e6a0661a2f0f6e436587fd
RuoYi <yzz_ivy@163.com>
2021-08-17 14:14:51 +0800

    支持自定义注解实现接口限流

---
+++ src/main/java/com/ruoyi/framework/aspectj/lang/enums/LimitType.java
@@ -0,0 +1,20 @@
+package com.ruoyi.framework.aspectj.lang.enums;
+
+/**
+ * 限流类型
+ *
+ * @author ruoyi
+ */
+
+public enum LimitType
+{
+    /**
+     * 默认策略全局限流
+     */
+    DEFAULT,
+
+    /**
+     * 根据请求者IP进行限流
+     */
+    IP
+}
