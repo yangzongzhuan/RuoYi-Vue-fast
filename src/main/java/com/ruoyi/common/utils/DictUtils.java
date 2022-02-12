98c62a9f5d38d106749609389577b52ddac5ae8a
RuoYi <yzz_ivy@163.com>
2022-02-12 12:47:11 +0800

    优化代码

--- src/main/java/com/ruoyi/common/utils/DictUtils.java
+++ src/main/java/com/ruoyi/common/utils/DictUtils.java
@@ -41,8 +41,7 @@ public class DictUtils
         Object cacheObj = SpringUtils.getBean(RedisCache.class).getCacheObject(getCacheKey(key));
         if (StringUtils.isNotNull(cacheObj))
         {
-            List<SysDictData> dictDatas = StringUtils.cast(cacheObj);
-            return dictDatas;
+            return StringUtils.cast(cacheObj);
         }
         return null;
     }
@@ -92,7 +91,7 @@ public class DictUtils
                 {
                     if (value.equals(dict.getDictValue()))
                     {
-                        propertyString.append(dict.getDictLabel() + separator);
+                        propertyString.append(dict.getDictLabel()).append(separator);
                         break;
                     }
                 }
@@ -132,7 +131,7 @@ public class DictUtils
                 {
                     if (label.equals(dict.getDictLabel()))
                     {
-                        propertyString.append(dict.getDictValue() + separator);
+                        propertyString.append(dict.getDictValue()).append(separator);
                         break;
                     }
                 }
