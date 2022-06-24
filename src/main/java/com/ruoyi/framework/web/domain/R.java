23a9bcbcb2fc3456c8c34d3cfab5db0c7d927e50
RuoYi <yzz_ivy@163.com>
2022-06-24 09:21:48 +0800

    调整响应成功状态码与全局保持一致

--- src/main/java/com/ruoyi/framework/web/domain/R.java
+++ src/main/java/com/ruoyi/framework/web/domain/R.java
@@ -1,6 +1,7 @@
 package com.ruoyi.framework.web.domain;
 
 import java.io.Serializable;
+import com.ruoyi.common.constant.HttpStatus;
 
 /**
  * 响应信息主体
@@ -12,10 +13,10 @@ public class R<T> implements Serializable
     private static final long serialVersionUID = 1L;
 
     /** 成功 */
-    public static final int SUCCESS = 0;
+    public static final int SUCCESS = HttpStatus.SUCCESS;
 
     /** 失败 */
-    public static final int FAIL = 500;
+    public static final int FAIL = HttpStatus.ERROR;
 
     private int code;
 
