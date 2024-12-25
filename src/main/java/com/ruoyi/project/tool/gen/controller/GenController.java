ed13fddc52995e8e6ec893a2a56f2b208e5b8db8
RuoYi <yzz_ivy@163.com>
2024-12-25 15:55:45 +0800

    代码生成新增配置是否允许文件覆盖到本地

--- src/main/java/com/ruoyi/project/tool/gen/controller/GenController.java
+++ src/main/java/com/ruoyi/project/tool/gen/controller/GenController.java
@@ -27,6 +27,7 @@ import com.ruoyi.common.utils.SecurityUtils;
 import com.ruoyi.common.utils.sql.SqlUtil;
 import com.ruoyi.framework.aspectj.lang.annotation.Log;
 import com.ruoyi.framework.aspectj.lang.enums.BusinessType;
+import com.ruoyi.framework.config.GenConfig;
 import com.ruoyi.framework.web.controller.BaseController;
 import com.ruoyi.framework.web.domain.AjaxResult;
 import com.ruoyi.framework.web.page.TableDataInfo;
@@ -213,6 +214,10 @@ public class GenController extends BaseController
     @GetMapping("/genCode/{tableName}")
     public AjaxResult genCode(@PathVariable("tableName") String tableName)
     {
+        if (!GenConfig.isAllowOverwrite())
+        {
+            return AjaxResult.error("【系统预设】不允许生成文件覆盖到本地");
+        }
         genTableService.generatorCode(tableName);
         return success();
     }
