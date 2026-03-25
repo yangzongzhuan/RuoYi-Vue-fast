5189c83b4fb47c95851ead0fb8fdfa25fae9dcbf
RuoYi <yzz_ivy@163.com>
2026-03-25 12:34:02 +0800

    修复typescript版多表代码生成报错问题

--- src/main/java/com/ruoyi/project/monitor/domain/SysJobLog.java
+++ src/main/java/com/ruoyi/project/monitor/domain/SysJobLog.java
@@ -3,7 +3,6 @@ package com.ruoyi.project.monitor.domain;
 import java.util.Date;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ruoyi.framework.aspectj.lang.annotation.Excel;
 import com.ruoyi.framework.web.domain.BaseEntity;
